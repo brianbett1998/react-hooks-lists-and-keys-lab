860037319f72e20abe625268db5bc3d863ff0aaa
BRIAN BETT <+>
2023-04-19 22:07:30 +0300

    Completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,18 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <ul id="project-list">
+        {/* render ProjectItem components here */}
+        {projects.map((project) => (
+          <li key={project.name}>
+            <ProjectItem
+              name={project.name}
+              about={project.about}
+              technologies={project.technologies}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
